c50670d15886805d7dc9f12350d2b06946be1ce7
aleksandrledenev <ledenevsanok@gmail.com>
2024-04-01 19:35:21 +0300

    Добавлен блютуз, сканирование устройств и их вывод на экран соединение

--- lib/activity/connection.dart
+++ lib/activity/connection.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
-import 'package:get/get.dart';
-import 'package:nalivator_applicatioin2/controllers/bluetooth.dart';
-
 
 class Connection extends StatefulWidget {
   const Connection({super.key});
@@ -12,60 +9,62 @@ class Connection extends StatefulWidget {
 }
 
 class _ConnectionState extends State<Connection> {
+  String _titleText = 'Соединение';
+
+  List<BluetoothDevice> devicesList = [];
+  FlutterBlue flutterBlue = FlutterBlue.instance;
+
+  void _startScanning() {
+    flutterBlue.startScan(timeout: const Duration(seconds: 4));
+    devicesList.clear();
+    flutterBlue.scanResults.listen((results) {
+      for (ScanResult r in results) {
+        if (!devicesList.contains(r.device)) {
+          setState(() {
+            devicesList.add(r.device);
+          });
+        }
+      }
+    });
+    flutterBlue.stopScan();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: const Text('Соединение'),
-          backgroundColor: Colors.green[400],
+        appBar: AppBar(
+          title: Text(_titleText),
         ),
-      body: GetBuilder<BleController>(
-        init: BleController(),
-        builder: (controller) {
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(height: 15,),
-                StreamBuilder<List<ScanResult>>(
-                    stream: controller.scanResults,
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        return ListView.builder(
-                          shrinkWrap: true,
-                            itemCount: snapshot.data!.length,
-                            itemBuilder: (context, index) {
-                              final data = snapshot.data![index];
-                              return Card(
-                                elevation: 2,
-                                child: ListTile(
-                                  title: Text(data.device.name),
-                                  subtitle: Text(data.device.id.id),
-                                  trailing: Text(data.rssi.toString()),
-                                ),
-                              );
-                            });
-                      }else{
-                          return const Text("No Device Found");
-                      }
-                    }),
-                ElevatedButton(
-                  onPressed: () =>controller.scanDevices(), 
-                  child: const Text(
-                    "Scan",
-                    style: TextStyle(fontSize: 60))
-                  ),
-                const SizedBox( height: 15,),
-              ],
+        body: Column(
+          children: <Widget>[
+            if(devicesList.isEmpty)
+            const Center(
+              child: Text('Ничего не найдено', style: TextStyle(fontSize: 35))),
+            Expanded(
+              child: ListView.builder(
+                itemCount: devicesList.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(devicesList[index].name),
+                    subtitle: Text(devicesList[index].id.toString()),
+                    onTap: () {
+                      print(devicesList[index]);
+                    },
+                  );
+                },
+              ),
             ),
-          );
-        },
-      ),
-    );
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: _startScanning, 
+            child: const Icon(Icons.search))
+            );
+  }
+
+  @override
+  void dispose() {
+    flutterBlue.stopScan();
+    super.dispose();
+  }
 }
-}
-  
-
-
-
--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,10 @@ void main() {
   runApp(const MainApp());
 }
 
+bool autoMode = false;
+bool leftMode = false;
 
-  bool autoMode = false;
-  bool leftMode = false;
-
-  bool isConnect = true;
- 
-  
-
+bool isConnect = false;
 
 class MainApp extends StatefulWidget {
   const MainApp({super.key});
@@ -26,9 +22,8 @@ class MainApp extends StatefulWidget {
 class _MainAppState extends State<MainApp> {
   int _currentPage = 1;
 
-  static const _colorGrayWindow =  Color(0xFF9DA1AA);
-  static const _colorDarkBlue =  Color(0xFF3B83BD);
-  
+  static const _colorGrayWindow = Color(0xFF9DA1AA);
+  static const _colorDarkBlue = Color(0xFF3B83BD);
 
   List<Widget> pageList = [
     const Connection(),
@@ -50,21 +45,24 @@ class _MainAppState extends State<MainApp> {
             items: const [
               BottomNavigationBarItem(
                   icon: Icon(Icons.contactless), label: "Соединение"),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.home), label: "Главная"),
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: "Главная"),
               BottomNavigationBarItem(
                   icon: Icon(Icons.settings), label: "Настройки"),
             ],
             onTap: (int intIndex) {
               setState(() {
                 _currentPage = intIndex;
+                
               });
             },
             enableFeedback: true,
             iconSize: 40,
-            selectedIconTheme: const IconThemeData(color: _colorDarkBlue, size: 35),
-            unselectedIconTheme: const IconThemeData(color: _colorGrayWindow, size: 30),
-            selectedLabelStyle: const TextStyle(fontSize: 15, fontWeight: FontWeight.w400),
+            selectedIconTheme:
+                const IconThemeData(color: _colorDarkBlue, size: 35),
+            unselectedIconTheme:
+                const IconThemeData(color: _colorGrayWindow, size: 30),
+            selectedLabelStyle:
+                const TextStyle(fontSize: 15, fontWeight: FontWeight.w400),
             unselectedLabelStyle: const TextStyle(fontSize: 12),
           )),
     );
